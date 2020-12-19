c487cfbe925aaaeda4143c37c1d0f6887fb165f2
JadeDragonhuxy <xy9258814879>
2020-12-19 10:30:37 +0800

    2020。12。19  10：30

--- src/main/java/com/example/basis/class01/PrintBinary.java
+++ src/main/java/com/example/basis/class01/PrintBinary.java
@@ -12,10 +12,16 @@ package com.example.basis.class01;
 public class PrintBinary {
 
     public static void printBinary(int num){
-        for (int i = 0; i <= 31; i++){
-            System.out.print((num & (1 << i)) == 1 ? "1" : "0");
+        for (int i = 31; i >= 0; i--){
+//            System.out.print((num & (1 << i)) == 1 ? "1" : "0");//不能判断等于1，因为在进行与运算以后除非结果为000000000000000000000000000000001与1进行二目运算能为true其余皆为false
+            System.out.print((num & (1 << i)) == 0 ? "0" : "1");
         }
+        System.out.println();
     }
 
+    public static void main(String[] args){
+        int num = 4;
+        printBinary(num);
+    }
 
 }
